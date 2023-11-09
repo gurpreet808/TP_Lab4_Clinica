18ddb9cefe205fc0a5d0c74a6db877a9a46f96d0
gurpreet808 <gurpreets808@gmail.com>
2023-11-09 04:08:15 -0300

    ruteo beta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaUsuariosComponent } from './componentes/lista-usuarios/lista-usuarios.component';
+import { ListaPacientesComponent } from './componentes/lista-pacientes/lista-pacientes.component';
+import { ListaEspecialistasComponent } from './componentes/lista-especialistas/lista-especialistas.component';
+import { ListaObrasSocialesComponent } from './componentes/lista-obras-sociales/lista-obras-sociales.component';
+import { ListaEspecialidadesComponent } from './componentes/lista-especialidades/lista-especialidades.component';
+import { HomeComponent } from './componentes/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'usuarios', component: ListaUsuariosComponent
+  },
+  {
+    path: 'pacientes', component: ListaPacientesComponent
+  },
+  {
+    path: 'especialistas', component: ListaEspecialistasComponent
+  },
+  {
+    path: 'obras-sociales', component: ListaObrasSocialesComponent
+  },
+  {
+    path: 'especialidades', component: ListaEspecialidadesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
