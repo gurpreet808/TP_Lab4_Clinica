0cb21809aa1d66b0ec7d589ffcc657bbd24ee0d1
gurpreet808 <gurpreets808@gmail.com>
2023-11-09 04:10:08 -0300

    Agrego servcios base

---
+++ src/app/servicios/especialidad.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Especialidad } from '../clases/especialidad';
+import { CollectionReference, DocumentData, Firestore, Query, collection, collectionData, deleteDoc, doc, setDoc } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EspecialidadService {
+
+  especialidades: BehaviorSubject<Especialidad[]> = new BehaviorSubject<Especialidad[]>([]);
+
+  pathUrl: string = 'especialidades';
+  dataRef: CollectionReference<DocumentData, DocumentData> = collection(this.firestore, this.pathUrl);
+
+  constructor(private firestore: Firestore) {
+    this.TraerEspecialidades();
+  }
+
+  TraerEspecialidades() {
+    let query: Query<Especialidad, DocumentData> = this.dataRef as Query<Especialidad, DocumentData>;
+
+    collectionData<Especialidad>(query, { idField: 'id' }).subscribe(
+      (especialidades: Especialidad[]) => {
+        this.especialidades.next(especialidades);
+      }
+    );
+  }
+
+  AgregarEspecialidad(especialidad: Especialidad) {
+    if (especialidad === null) {
+      return Promise.reject('Especialidad nula');
+    };
+
+    //if name is already in use
+    if (this.especialidades.value.find((os: Especialidad) => os.nombre === especialidad.nombre)) {
+      return Promise.reject('Ya se agregó esa especialidad');
+    };
+
+    let docRef = doc(this.dataRef);
+    especialidad.id = docRef.id;
+    return setDoc(docRef, especialidad);
+  }
+
+  ModificarEspecialidad(especialidad: Especialidad) {
+    if (especialidad === null) {
+      return Promise.reject('Especialidad nula');
+    };
+
+    //if name is already in use
+    if (this.especialidades.value.find((os: Especialidad) => os.nombre === especialidad.nombre)) {
+      return Promise.reject('Ya se agregó esa especialidad');
+    };
+
+    let docRef = doc(this.dataRef, especialidad.id);
+    return setDoc(docRef, especialidad);
+  }
+
+  BorrarEspecialidad(id: string) {
+    if (id === null) {
+      return Promise.reject('ID nulo');
+    };
+
+    let docRef = doc(this.dataRef, id);
+    return deleteDoc(docRef);
+  }
+
+  ToogleValidarEspecialidad(especialidad: Especialidad) {
+    if (especialidad === null) {
+      return Promise.reject('Especialidad nula');
+    };
+
+    let docRef = doc(this.dataRef, especialidad.id);
+    especialidad.valida = !especialidad.valida;
+    return setDoc(docRef, especialidad);
+  }
+}
