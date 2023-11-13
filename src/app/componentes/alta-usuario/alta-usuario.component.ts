1593686eb2b28c0fdceb3025cacf85c0735278c3
gurpreet <gurpreets808@gmail.com>
2023-11-13 18:03:56 -0300

    Alta usuario que reciba por input tipo de usuario

--- src/app/componentes/alta-usuario/alta-usuario.component.ts
+++ src/app/componentes/alta-usuario/alta-usuario.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-alta-usuario',
@@ -7,19 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltaUsuarioComponent implements OnInit {
 
-  tiposUsuario: string[] = [
-    'admin',
-    'paciente',
-    'especialista'
-  ];
+  @Input() tipo_usuario: string = '';
   especialidades: string[] = [];
-  especialidad: string = '';
-  tipo_usuario: string = '';
-  
-  constructor() {
+
+  adminForm: FormGroup = this.formBuilder.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required, Validators.minLength(6)]],
+      clave2: ['', [Validators.required, Validators.minLength(6)]],
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      apellido: ['', [Validators.required, Validators.minLength(2)]],
+      dni: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(8)]],
+      edad: ['', [Validators.required, Validators.min(1), Validators.max(99)]],
+      url_foto_1: ['', [Validators.required]]
+    }
+  );
+  pacienteForm: FormGroup = new FormGroup(this.adminForm.controls);
+  especialistaForm: FormGroup = new FormGroup(this.adminForm.controls);
+
+  constructor(private formBuilder: FormBuilder) {
+    this.InicializarFormularios();
   }
 
   ngOnInit(): void {
+  }
+
+  InicializarFormularios() {
+    //const adminFormGroupString = JSON.stringify(this.adminForm);
+    //const adminFormGroupJSON = JSON.parse(adminFormGroupString);
+
+    //this.pacienteForm = new FormGroup(adminFormGroupJSON);
+
+    this.pacienteForm.addControl('url_foto_2', new FormControl('', [Validators.required]));
+    this.pacienteForm.addControl('obra_social', new FormControl('', [Validators.required]));
+
+    //this.especialistaForm = new FormGroup(adminFormGroupJSON);
+
+    this.especialistaForm.addControl('habilitado', new FormControl('', [Validators.required]));
+    this.especialistaForm.addControl('especialidades', new FormControl('', [Validators.required]));
   }
 
 }
--- src/app/pages/registrarse/registrarse.component.ts
+++ src/app/pages/registrarse/registrarse.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registrarse',
   templateUrl: './registrarse.component.html',
   styleUrls: ['./registrarse.component.scss']
 })
-export class RegistrarseComponent {
+export class RegistrarseComponent implements OnInit{
+  tipo_usuario: string = '';
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  SeleccionarTipo(tipo_usuario: string){
+    console.log(tipo_usuario);
+    this.tipo_usuario = tipo_usuario;
+  }
 }
