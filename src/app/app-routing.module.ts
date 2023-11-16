0a6db5caa54f4d22d5c44daab4d2fa24f08496dc
gurpreet808 <gurpreets808@gmail.com>
2023-11-16 20:33:17 -0300

    Agrego error404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'usuarios', loadChildren: () => import('./pages/usuarios/usuarios.module').then(m => m.UsuariosModule) },
   { path: 'obras-sociales', loadChildren: () => import('./pages/obras-sociales/obras-sociales.module').then(m => m.ObrasSocialesModule) },
   { path: 'especialidades', loadChildren: () => import('./pages/especialidades/especialidades.module').then(m => m.EspecialidadesModule) },
+  { path: '**', loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module) }
 ];
 
 @NgModule({
