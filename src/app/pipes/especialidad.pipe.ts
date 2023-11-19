58c709dab48ff03a66e6963e96380fe0a40ca614
gurpreet <gurpreets808@gmail.com>
2023-11-18 22:34:38 -0300

    Mejora en pipes usando async

--- src/app/pipes/especialidad.pipe.ts
+++ src/app/pipes/especialidad.pipe.ts
@@ -1,12 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { EspecialidadService } from '../servicios/especialidad.service';
+import { Especialidad } from '../clases/especialidad';
+import { Observable, filter, map } from 'rxjs';
 
 @Pipe({
   name: 'especialidad'
 })
 export class EspecialidadPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  constructor(private servEspecialidad: EspecialidadService) { }
+
+  transform(value: string): Observable<string> {
+    return this.servEspecialidad.especialidades.pipe(
+      // Filtra el valor inicial para evitar procesarlo
+      filter(especialidades => especialidades.length > 0),
+      map((especialidades: Especialidad[]) => {
+        const obraSocial = especialidades.find((os: Especialidad) => os.id === value);
+        return obraSocial ? obraSocial.nombre : 'No encontrada';
+      })
+    );
   }
 
 }
--- src/app/pipes/obra-social.pipe.ts
+++ src/app/pipes/obra-social.pipe.ts
@@ -1,27 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ObraSocialService } from '../servicios/obra-social.service';
 import { ObraSocial } from '../clases/obra-social';
+import { Observable, filter, map } from 'rxjs';
 
 @Pipe({
-  name: 'obraSocial',
-  pure: false
+  name: 'obraSocial'
 })
 export class ObraSocialPipe implements PipeTransform {
 
-  constructor(private servObraSocial: ObraSocialService) {
+  constructor(private servObraSocial: ObraSocialService) { }
 
-  }
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    if (value != undefined) {
-      let obra_social = this.servObraSocial.obras_sociales.value.find((os: ObraSocial) => os.id === value);
-
-      if (obra_social) {
-        return obra_social.nombre;
-      }
-    }
-    //return "Obra Social no encontrada";
-    return value;
+  transform(value: string): Observable<string> {
+    return this.servObraSocial.obras_sociales.pipe(
+      // Filtra el valor inicial para evitar procesarlo
+      filter(obrasSociales => obrasSociales.length > 0),
+      map((obrasSociales: ObraSocial[]) => {
+        const obraSocial = obrasSociales.find((os: ObraSocial) => os.id === value);
+        return obraSocial ? obraSocial.nombre : 'No encontrada';
+      })
+    );
   }
 
 }
