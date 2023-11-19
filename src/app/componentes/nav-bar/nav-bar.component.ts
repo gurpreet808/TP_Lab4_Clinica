aa44ebf6d5cfecdae73e413edc75cbff161f9740
gurpreet <gurpreets808@gmail.com>
2023-11-18 22:49:18 -0300

    Agrego menu especialista, falta resolver suscribes

--- src/app/componentes/nav-bar/nav-bar.component.ts
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { skip } from 'rxjs';
 import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
@@ -14,38 +15,44 @@ export class NavBarComponent implements OnInit {
       label: 'Home',
       icon: 'fa-solid fa-house',
       routerLink: '/'
-    },
-    {
-      label: 'Usuarios',
-      icon: 'fa-solid fa-users-gear',
-      routerLink: '/usuarios'
-    },
-    {
-      label: 'Especialistas',
-      icon: 'fa-solid fa-user-doctor',
-      routerLink: '/especialistas'
-    },
-    {
-      label: 'Pacientes',
-      icon: 'fa-solid fa-users',
-      routerLink: '/pacientes'
-    },
-    {
-      label: 'Especialidades',
-      icon: 'fa-solid fa-stethoscope',
-      routerLink: '/especialidades'
-    },
-    {
-      label: 'Obras sociales',
-      icon: 'fa-solid fa-briefcase-medical',
-      routerLink: '/obras-sociales'
     }
   ];
 
-  ngOnInit(): void {
+  constructor(public servAuth: AuthService, private router: Router) {
+    this.AdminItems();
+    /* this.servAuth.logueado.pipe(
+      skip(1)
+    ).subscribe(
+      (logueado: boolean) => {
+        //console.log("logueado", this.servAuth.usuarioActual);
+  
+        if (logueado == true) {
+          switch (this.servAuth.usuarioActual?.tipo) {
+            case 'admin':
+              console.log("admin");
+              this.AdminItems();
+              break;
+  
+            case 'paciente':
+              console.log("paciente");
+              this.PacienteItems();
+              break;
+  
+            case 'especialista':
+              console.log("especialista");
+              this.EspecialistaItems();
+              break;
+  
+            default:
+              break;
+          }
+        }
+      }
+    ); */
   }
 
-  constructor(public servAuth: AuthService, private router: Router) { }
+  ngOnInit(): void {
+  }
 
   Desloguear() {
     this.servAuth.LogOut();
@@ -87,7 +94,32 @@ export class NavBarComponent implements OnInit {
     ];
   }
 
-  PacientesItems() {
+  PacienteItems() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'fa-solid fa-house',
+        routerLink: '/'
+      },
+      {
+        label: 'Mis turnos',
+        icon: 'fa-solid fa-users-gear',
+        routerLink: '/usuarios'
+      },
+      {
+        label: 'Histora clínica',
+        icon: 'fa-solid fa-user-doctor',
+        routerLink: '/especialistas'
+      },
+      {
+        label: 'Mis datos',
+        icon: 'fa-solid fa-users',
+        routerLink: '/pacientes'
+      }
+    ];
+  }
+
+  EspecialistaItems() {
     this.items = [
       {
         label: 'Home',
