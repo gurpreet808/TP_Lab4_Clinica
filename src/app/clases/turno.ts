a45e9a54dfc8af6f33750eba37640ab63c676f27
gurpreet <gurpreets808@gmail.com>
2023-11-21 21:09:18 -0300

    Creo campos de entidad turno

--- src/app/clases/turno.ts
+++ src/app/clases/turno.ts
@@ -1,2 +1,10 @@
 export interface Turno {
+    id: number;
+    id_paciente: number;
+    id_especialista: number;
+    id_estado: number;
+    fecha: string;
+    hora: string;
+    duracion: number;
+    especialidad: string;
 }
