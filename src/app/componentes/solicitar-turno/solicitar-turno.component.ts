3fb06fc790cacd760f52be883d80bdcadac247af
gurpreet <gurpreets808@gmail.com>
2023-11-29 17:45:03 -0300

    Primer versiòn de solicitar turno

--- src/app/componentes/solicitar-turno/solicitar-turno.component.ts
+++ src/app/componentes/solicitar-turno/solicitar-turno.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { filter, map } from 'rxjs';
+import { Especialidad } from 'src/app/clases/especialidad';
 import { Especialista } from 'src/app/clases/especialista';
+import { Paciente } from 'src/app/clases/paciente';
+import { AuthService } from 'src/app/servicios/auth.service';
 import { DisponibilidadService } from 'src/app/servicios/disponibilidad.service';
 import { EspecialidadService } from 'src/app/servicios/especialidad.service';
+import { SpinnerService } from 'src/app/servicios/spinner.service';
 import { TurnoService } from 'src/app/servicios/turno.service';
 import { UsuarioService } from 'src/app/servicios/usuario.service';
 
@@ -13,24 +17,60 @@ import { UsuarioService } from 'src/app/servicios/usuario.service';
 })
 export class SolicitarTurnoComponent implements OnInit {
 
+  paciente: Paciente | undefined;
   especialistas: Especialista[] = [];
   especialista: Especialista | undefined;
-  especialidad: string = '';
+  especialidades: Especialidad[] = [];
+  especialidad_id: string = '';
 
   constructor(
+    public servAuth: AuthService,
     public servUsuario: UsuarioService,
     public servEspecialidad: EspecialidadService,
     public servDisponibilidad: DisponibilidadService,
-    public servTurno: TurnoService
+    public servTurno: TurnoService,
+    public servSpinner: SpinnerService
   ) {
-    //suscribe to this.servUsuario.especialistas and set this.especialistas, but only if they are habilitado == true
+
+    this.servAuth.logueado.subscribe(
+      (logueado) => {
+        if (this.servAuth.usuarioActual && this.servAuth.usuarioActual.tipo === 'paciente') {
+          this.paciente = this.servAuth.usuarioActual as Paciente;
+          console.log(this.paciente);
+        }
+      }
+    );
+
+    //Hacer como lista de precios 2 suscribes con el Merge() que en este caso filtraría los especiaistas si sus especialidades son válidas y si hay diponibilidad para esa especialidades
+    this.servEspecialidad.especialidades.subscribe(
+      (especialidades) => {
+        this.especialidades = especialidades.filter((especialidad) => especialidad.valida);
+      }
+    );
+
     this.servUsuario.especialistas.subscribe(
       (especialistas) => {
-        this.especialistas = especialistas.filter(
-          (especialista) => {
-            return especialista.habilitado;
+        for (let i = 0; i < especialistas.length; i++) {
+          if (especialistas[i].habilitado) {
+
+            this.especialistas.push(especialistas[i]);
+
+            /* let especialidades: Especialidad[] = [];
+            especialistas[i].especialidades.forEach((id) => {
+              let especialidad = this.servEspecialidad.especialidades.value.find((especialidad) => especialidad.id === id);
+              if (especialidad && especialidad.valida) {
+                especialidades.push(especialidad);
+              }
+            });
+
+            console.log(especialidades);
+
+            if (especialidades.length > 0) {
+              especialistas[i].especialidades = especialidades.map((especialidad) => especialidad.id);
+              this.especialistas.push(especialistas[i]);
+            } */
           }
-        );
+        }
       }
     );
   }
@@ -38,18 +78,37 @@ export class SolicitarTurnoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ElegirPaciente(_paciente: Paciente) {
+    this.paciente = _paciente;
+    console.log(this.paciente);
+    this.especialista = undefined;
+    this.especialidad_id = '';
+  }
+
+  CancelarPaciente() {
+    this.paciente = undefined;
+    this.especialista = undefined;
+    this.especialidad_id = '';
+  }
+
   ElegirEspecialista(_especialista: Especialista) {
     this.especialista = _especialista;
     console.log(this.especialista);
+    this.especialidad_id = '';
   }
 
-  ElegirEspecialidad(_especialidad: string) {
-    this.especialidad = _especialidad;
-    console.log(this.especialidad);
+  CancelarEspecialista() {
+    this.especialista = undefined;
+    this.especialidad_id = '';
+  }
 
-    if (this.especialista && this.especialista.disponibilidades && this.especialista.disponibilidades.length > 0 && this.especialidad) {
+  ElegirEspecialidad(_especialidad_id: string) {
+    this.especialidad_id = _especialidad_id;
+    console.log(this.especialidad_id);
 
-      this.servDisponibilidad.DisponibilidadEspecialidadDeEspecialistaPorDia(1, this.especialidad, this.especialista.disponibilidades).then((disponibilidades) => {
+    if (this.especialista && this.especialista.disponibilidades && this.especialista.disponibilidades.length > 0 && this.especialidad_id) {
+
+      this.servDisponibilidad.DisponibilidadEspecialidadDeEspecialistaPorDia(1, this.especialidad_id, this.especialista.disponibilidades).then((disponibilidades) => {
         console.log(disponibilidades);
       }
       ).catch((error) => {
@@ -57,5 +116,9 @@ export class SolicitarTurnoComponent implements OnInit {
       });
 
     }
+  }
+
+  FiltrarEspecialistas() {
+    //check if 
   }
 }
