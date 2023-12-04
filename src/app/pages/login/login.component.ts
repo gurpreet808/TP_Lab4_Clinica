6cb488d8d5c7978b0c8bcdce56769947f73d32c9
gurpreet <gurpreets808@gmail.com>
2023-12-04 00:49:40 -0300

    FIx rutas viejas fotos para agregar con suscribe

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -15,10 +15,10 @@ export class LoginComponent {
   clave: string = "";
 
   usuariosMock = [
-    { nombre: "Esteban Quito", mail: "esteban@cj.MintEmail.com", tipo: "paciente", clave: "1q2w3e4r5t", foto: "https://firebasestorage.googleapis.com/v0/b/tpfinal-lab4-singh.appspot.com/o/images%2Fusuarios%2F1UfYiHuT6R3Ty9Qz4qdQ%2Fhombre2.jpg?alt=media&token=640024f7-65bb-4ac7-ba85-42542cbb53b2" },
-    { nombre: "Elsa Pallo", mail: "elsa@cj.MintEmail.com", tipo: "paciente", clave: "1q2w3e4r5t", foto: "https://firebasestorage.googleapis.com/v0/b/tpfinal-lab4-singh.appspot.com/o/images%2Fusuarios%2FlPujIbfTFORdeCVMWE6I5eFE0nz2%2Fmujer1.jpg?alt=media&token=4b466048-2156-4aa9-ac4f-60aa0ab44b85" },
+    { nombre: "Esteban Quito", mail: "esteban@cj.MintEmail.com", tipo: "paciente", clave: "1q2w3e4r5t", foto: "" },
+    { nombre: "Elsa Pallo", mail: "elsa@cj.MintEmail.com", tipo: "paciente", clave: "1q2w3e4r5t", foto: "" },
     { nombre: "Elvis Tek", mail: "elvis@cj.MintEmail.com", tipo: "especialista", clave: "1q2w3e4r5t" },
-    { nombre: "Armando Paredes", mail: "admin@cj.MintEmail.com", tipo: "admin", clave: "1q2w3e4r5t", foto: "https://firebasestorage.googleapis.com/v0/b/tpfinal-lab4-singh.appspot.com/o/images%2Fusuarios%2FYYk4sbb0nOXhPf2f4xFHUr35B6b2%2Fadmin.jpg?alt=media&token=fac36d7c-63d1-4a47-ae96-db7e4853ecbc" },
+    { nombre: "Armando Paredes", mail: "admin@cj.MintEmail.com", tipo: "admin", clave: "1q2w3e4r5t", foto: "" },
   ]
 
   constructor(public servAuth: AuthService, private router: Router, public messageService: MessageService, public servSpinner: SpinnerService, public servUsuario: UsuarioService) {
